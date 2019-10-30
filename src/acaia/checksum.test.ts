1bd1e5a158672273ec9e9feff48d7afe1ba1de1d
Oscar Bolmsten <oscar@bolmsten.se>
2019-10-30 23:32:34 +0100

    Now the tests actually work And should fail with current code

--- src/acaia/checksum.test.ts
+++ src/acaia/checksum.test.ts
@@ -1,22 +1,20 @@
 import { validateChecksum } from './checksum';
 
 describe('validateChecksum', () => {
-	test('validates packets successfully', () => {
-		const packets = [
-			new Uint8Array([239, 221, 12, 8, 5, 4, 0, 0, 0, 2, 2, 14, 7]),
-			new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 1, 15, 49]),
-			new Uint8Array([239, 221, 7, 7, 2, 20, 2, 60, 5, 0, 87, 9]),
-		];
-		packets.forEach(packet => expect(validateChecksum(packet)).toBeTruthy);
+	test.each([
+		new Uint8Array([239, 221, 12, 8, 5, 4, 0, 0, 0, 2, 2, 14, 7]),
+		new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 1, 15, 49]),
+		new Uint8Array([239, 221, 7, 7, 2, 20, 2, 60, 5, 0, 87, 9]),
+	])('successfully validates checksum for packet %#', packet => {
+		expect(validateChecksum(packet)).toBe(true);
 	});
 
-	test('fails checksum check for damaged packets', () => {
-		const packets = [
-			new Uint8Array([239, 221, 12, 8, 4, 5, 0, 0, 0, 2, 2, 14, 7]),
-			new Uint8Array([239, 221, 12, 8, 4, 5, 0, 0, 0, 2, 2, 14]),
-			new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 0, 15, 49]),
-			new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 0, 0, 49]),
-		];
-		packets.forEach(packet => expect(validateChecksum(packet)).toBeFalsy);
+	test.each([
+		new Uint8Array([239, 221, 12, 8, 4, 5, 0, 0, 0, 2, 2, 14, 7]),
+		new Uint8Array([239, 221, 12, 8, 4, 5, 0, 0, 0, 2, 2, 14]),
+		new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 0, 15, 49]),
+		new Uint8Array([239, 221, 8, 9, 47, 2, 2, 3, 0, 0, 0, 0, 0, 49]),
+	])('fails checksum check for damaged packet %#', packet => {
+		expect(validateChecksum(packet)).toBe(false);
 	});
 });
