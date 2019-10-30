109981c48c586f40f661eeee092b79e50e9e1a4a
Oscar Bolmsten <oscar@bolmsten.se>
2019-10-30 23:41:59 +0100

    Calculate separate checksum for even/odd bytes As that's what the packet checksum (last two bytes) represent. This will detect errors where two bytes are switched in the wrong order.

--- src/acaia/checksum.ts
+++ src/acaia/checksum.ts
@@ -1,16 +1,23 @@
 import { HEADER_LENGTH, CHECKSUM_LENGTH } from './constants';
 
 const validateChecksum = (packet: Uint8Array): boolean => {
-	const calculatedChecksum = checksum(
-		packet.slice(HEADER_LENGTH, packet.length - CHECKSUM_LENGTH),
+	const payload = packet.slice(
+		HEADER_LENGTH,
+		packet.length - CHECKSUM_LENGTH,
 	);
+	const evenChecksum = checksum(payload.filter(isEven));
+	const oddChecksum = checksum(payload.filter(isOdd));
 
-	const packetChecksum = checksum(packet.slice(CHECKSUM_LENGTH * -1));
-
-	return calculatedChecksum === packetChecksum;
+	return (
+		evenChecksum === packet[packet.length - 2] &&
+		oddChecksum === packet[packet.length - 1]
+	);
 };
 
 const checksum = (data: Uint8Array): number =>
 	data.reduce((prev, curr) => prev + curr, 0) & 0xff;
+
+const isOdd = (v: number, index: number) => index % 2;
+const isEven = (v: number, index: number) => !(index % 2);
 
 export { validateChecksum };
